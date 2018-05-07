70434e834c4bbd7b7ade6e6ed6ebbf8a663b5ffb
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-05-07 21:26:46 +1000

    Added untimed 'stage' obstacle.

---
+++ Assets/Untimed_Trigger_Event_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Untimed_Trigger_Event_Script : MonoBehaviour {
+
+    public GameObject playerObject;
+    public GameObject lockedObject;
+
+    private Animator lockedObjectAnimator;
+
+    // Use this for initialization
+    void Start () {
+        lockedObjectAnimator = lockedObject.GetComponent<Animator>();
+
+        try {
+
+            playerObject = GameObject.Find("Player");
+
+        } catch {
+            playerObject = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject == playerObject) {
+            lockedObjectAnimator.Play("doorOpening");
+        }
+    }
+}
