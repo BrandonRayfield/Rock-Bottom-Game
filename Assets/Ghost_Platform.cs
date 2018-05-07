2295429d37e7602e79e3a4de89e0e7101d96920b
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-05-07 21:24:36 +1000

    Added new type of platform the player is able to jump through. Still need to make it so they can fall though them by pressing the down key.

---
+++ Assets/Ghost_Platform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost_Platform : MonoBehaviour {
+
+    public GameObject playerObject;
+    public GameObject platformObject;
+    private Collider platformCollider;
+
+    // Use this for initialization
+    void Start () {
+        try {
+
+            playerObject = GameObject.Find("Player");
+            platformObject = this.gameObject.transform.GetChild(0).gameObject;
+            platformCollider = platformObject.GetComponent<Collider>();
+
+        } catch {
+            playerObject = null;
+            platformObject = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject == playerObject) {
+            platformCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject == playerObject) {
+            platformCollider.enabled = true;
+        }
+    }
+
+}
