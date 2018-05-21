d507e035604bdc602e88a1a401719297bb0b820e
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-05-21 20:41:53 +1000

    Updated trigger platforms.Now the player has to press 'E' to play the guitar to trigger them. Cutscene plays showing player what door opened

--- Assets/Untimed_Trigger_Event_Script.cs
+++ Assets/Untimed_Trigger_Event_Script.cs
@@ -6,7 +6,13 @@ public class Untimed_Trigger_Event_Script : MonoBehaviour {
 
     public GameObject playerObject;
     public GameObject lockedObject;
+    private GameObject cameraObject;
 
+    private bool isTouching;
+    private float time;
+    private bool padTriggered;
+
+    private Animator playerAnimator;
     private Animator lockedObjectAnimator;
 
     // Use this for initialization
@@ -16,20 +22,43 @@ public class Untimed_Trigger_Event_Script : MonoBehaviour {
         try {
 
             playerObject = GameObject.Find("Player");
+            cameraObject = GameObject.Find("Camera");
+            playerAnimator = playerObject.GetComponent<Animator>();
 
         } catch {
             playerObject = null;
+            cameraObject = null;
+            playerAnimator = null;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update() {
+        if (isTouching && Input.GetKeyDown(KeyCode.E)) {
+            playerAnimator.Play("Guitar Playing");
+            padTriggered = true;
+        }
+
+        if (padTriggered) {
+            time += Time.deltaTime;
+            if (time >= 1) {
+                cameraObject.GetComponent<Camera>().SetFocusPoint(lockedObject);
+                lockedObjectAnimator.Play("doorOpening");
+                time = 0;
+                padTriggered = false;
+            }
+        }
 	}
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject == playerObject) {
-            lockedObjectAnimator.Play("doorOpening");
+            isTouching = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject == playerObject) {
+            isTouching = false;
         }
     }
 }
