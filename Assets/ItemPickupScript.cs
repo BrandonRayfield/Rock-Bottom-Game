cab256ec3120627fa4cf6ef2b1f06d8d627d1cf7
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-05-21 20:36:46 +1000

    Added Collectable Objects into game. Currently have coins and shards (which might be used in an NPC quest)

---
+++ Assets/ItemPickupScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickupScript : MonoBehaviour {
+
+    public int value;
+    public int itemID;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+}
