52c284e8f2712f8ebe23a136e98466ac3d9ea205
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-07-29 14:47:37 +1000

    Implemented dynamic dialogue system, added Christina's character portraits. Changed how guitar model works with player model. Moved some files around (hopefully doesn't clash with Dan's)

---
+++ Assets/Scripts/Dialogue/Line.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Line {
+
+    [TextArea]
+    public string characterName;
+
+    [TextArea(3,10)]
+    public string speech;
+
+    public Texture2D characterPortrait;
+
+    public bool isDecision;
+}
