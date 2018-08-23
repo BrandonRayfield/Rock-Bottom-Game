6d9fcdf85394fdf37e228a090e6017f484da1929
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-08-24 00:40:03 +1000

    Updated dark trigger so background music also becomes softer

--- Assets/Scripts/Environmental_Obstacles/Lighting/Light_Control.cs
+++ Assets/Scripts/Environmental_Obstacles/Lighting/Light_Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Light_Control : MonoBehaviour {
 
     public Light mainLight;
+    public GameObject mainCamera;
     public bool isDark;
     public int dimSpeed = 5;
 
@@ -12,21 +13,37 @@ public class Light_Control : MonoBehaviour {
     private float minIntensity = 0.0f;
     public float currentIntensity;
 
-	// Use this for initialization
-	void Start () {
+    private float maxVolume;
+    private float minVolume;
+    public float currentVolume;
+
+    // Use this for initialization
+    void Start () {
+        try {
+            mainCamera = GameObject.Find("Camera");
+        } catch {
+            mainCamera = null;
+        }
+
+        maxVolume = mainCamera.GetComponent<AudioSource>().volume;
+        minVolume = maxVolume / 2;
         currentIntensity = maxIntensity;
-	}
+        currentVolume = maxVolume;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         mainLight.intensity = currentIntensity;
+        mainCamera.GetComponent<AudioSource>().volume = currentVolume;
 
         if (isDark) {
+            currentVolume = minVolume;
             if (currentIntensity > minIntensity) {
                 currentIntensity -= (Time.deltaTime / dimSpeed);
             }
         } else {
+            currentVolume = maxVolume;
             if (currentIntensity < maxIntensity) {
                 currentIntensity += (Time.deltaTime / dimSpeed);
             }
