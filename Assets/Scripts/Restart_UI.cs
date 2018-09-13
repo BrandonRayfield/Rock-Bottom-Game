8312077f6e4beea7a936a28c006cbe179599ec6d
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-09-14 05:02:26 +1000

    Script for restarting game over menu. Forgot to include it

---
+++ Assets/Scripts/Restart_UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart_UI : MonoBehaviour {
+
+    private int currentLevel;
+    private int mainMenu = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void restartCurrentLevel() {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    public void returnToMainMenu() {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
