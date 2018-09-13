05194f864e9bbc8a77ce185c3a255a06010772ce
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-09-14 04:57:21 +1000

    All checkpoints now restore player to full health the first time they are reached

--- Assets/Scripts/CheckpointScript.cs
+++ Assets/Scripts/CheckpointScript.cs
@@ -4,14 +4,7 @@ using UnityEngine.UI;
 
 public class CheckpointScript : MonoBehaviour {
 
-    public Text checkpointText;
-    private bool firstTimecheckpointReached = false;
-    private bool textDisappeared = false;
-    private float time;
-    private float maxTime = 3.0f;
-
-    public string message = "Checkpoint Reached!";
-
+    private bool checkpointActive;
 
     // Use this for initialization
     void Start () {
@@ -20,31 +13,13 @@ public class CheckpointScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (textDisappeared)
-        {
-            time = 0;
-        }
 
-        if (firstTimecheckpointReached && !textDisappeared) {
-            time += Time.deltaTime;
-
-            if (time > maxTime)
-            {
-                checkpointText.enabled = false;
-                textDisappeared = true;
-            }
-        }
 	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.transform.tag == "Player" && !firstTimecheckpointReached)
-        {
-            time = 0;
-            checkpointText.text = message;
-            checkpointText.enabled = true;
-            firstTimecheckpointReached = true;
-            textDisappeared = false;
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.tag == "Player" && !checkpointActive) {
+            checkpointActive = true;
+            other.GetComponent<Player>().gainHealth(100);
         }
     }
 }
