c4c59c2983c660ae6d69e10f5ff4f413e2294eb3
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2018-10-12 06:05:52 +1000

    More scripts for weapon abilities

---
+++ Assets/Scripts/Explosive_Ability.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive_Ability : MonoBehaviour {
+
+    private float time = 0.0f;
+    private float lifeTime = 3.0f;
+    private float damage;
+
+    // Use this for initialization
+    void Start() {
+        damage = 100;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+        if (time >= lifeTime)
+            Destroy(this.gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy") {
+            var enemy = other.gameObject.GetComponent<Enemy>();
+            enemy.takeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/FireDamage.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDamage : MonoBehaviour {
+
+    public float lifeTime = 0.1f;
+
+    public float damage = 25.0f;
+
+    public bool player = true;
+
+    private bool hasDamaged;
+
+    public GameObject guitarPoint;
+
+    // Use this for initialization
+    void Start() {
+        Destroy(this.gameObject, lifeTime);
+        hasDamaged = false;
+    }
+
+    private void Update() {
+
+    }
+
+
+    //private void OnParticleCollision(GameObject otherObject) {
+    //    if (player) {
+    //        if (otherObject.transform.tag == "Enemy") {
+    //                if (otherObject.GetComponent<Enemy>() != null)
+    //                    otherObject.GetComponent<Enemy>().takeDamage(damage);
+    //                else if (otherObject.GetComponent<Chaser>() != null)
+    //                    otherObject.GetComponent<Chaser>().takeDamage(damage);
+    //                else if (otherObject.GetComponent<Boss>() != null)
+    //                    otherObject.GetComponent<Boss>().takeDamage(damage);
+    //                else if (otherObject.GetComponent<Large_Spider>() != null)
+    //                    otherObject.GetComponent<Large_Spider>().takeDamage(damage);
+    //        }
+    //        //----------------------------------------------------------------------------------
+    //        else if (otherObject.transform.tag == "FlyingEnemy") {
+    //                if (otherObject.GetComponent<Flying_Enemy>())
+    //                    otherObject.GetComponent<Flying_Enemy>().takeDamage(damage);
+    //                else if (otherObject.GetComponent<Flying_Crush_Enemy>())
+    //                    otherObject.GetComponent<Flying_Crush_Enemy>().takeDamage(damage);
+    //                else if (otherObject.GetComponent<Flying_Kamikaze_Enemy>())
+    //                    otherObject.GetComponent<Flying_Kamikaze_Enemy>().takeDamage(damage);
+    //        }
+    //        //----------------------------------------------------------------------------------
+    //        else if (otherObject.transform.tag == "Ranged_Enemy") {
+    //                otherObject.GetComponent<Ranged_Enemy>().takeDamage(damage);
+    //        }
+    //        //----------------------------------------------------------------------------------
+    //        else if (otherObject.transform.tag == "Ranged_Enemy2") {
+    //                otherObject.GetComponent<Ranged_Enemy2>().takeDamage(damage);
+    //        }
+    //        //----------------------------------------------------------------------------------
+    //        else if (otherObject.transform.tag == "Chaser") {
+    //                otherObject.GetComponent<Chaser>().takeDamage(damage);
+    //        }
+    //    }
+    //}
+
+    void OnTriggerStay(Collider otherObject) {
+
+        RaycastHit hit;
+
+        if (player) {
+            if (otherObject.transform.tag == "Enemy") {
+                if (Physics.Raycast(guitarPoint.transform.position, -(guitarPoint.transform.position - otherObject.transform.position).normalized, out hit,
+                                                            Vector3.Distance(guitarPoint.transform.position, otherObject.transform.position))) {
+                    if (hit.transform.GetComponent<Enemy>() != null)
+                        hit.transform.GetComponent<Enemy>().takeDamage(damage);
+                    else if (hit.transform.GetComponent<Chaser>() != null)
+                        hit.transform.GetComponent<Chaser>().takeDamage(damage);
+                    else if (hit.transform.GetComponent<Boss>() != null)
+                        hit.transform.GetComponent<Boss>().takeDamage(damage);
+                    else if (hit.transform.GetComponent<Large_Spider>() != null)
+                        hit.transform.GetComponent<Large_Spider>().takeDamage(damage);
+                }
+            }
+            //----------------------------------------------------------------------------------
+            else if (otherObject.transform.tag == "FlyingEnemy") {
+                if (Physics.Raycast(guitarPoint.transform.position, -(guitarPoint.transform.position - otherObject.transform.position).normalized, out hit,
+                                                                Vector3.Distance(guitarPoint.transform.position, otherObject.transform.position))) {
+                    if (otherObject.GetComponent<Flying_Enemy>())
+                        otherObject.GetComponent<Flying_Enemy>().takeDamage(damage);
+                    else if (otherObject.GetComponent<Flying_Crush_Enemy>())
+                        otherObject.GetComponent<Flying_Crush_Enemy>().takeDamage(damage);
+                    else if (otherObject.GetComponent<Flying_Kamikaze_Enemy>())
+                        otherObject.GetComponent<Flying_Kamikaze_Enemy>().takeDamage(damage);
+                }
+            }
+            //----------------------------------------------------------------------------------
+            else if (otherObject.transform.tag == "Ranged_Enemy") {
+                if (!Physics.Raycast(guitarPoint.transform.position, -(guitarPoint.transform.position - otherObject.transform.position).normalized, out hit,
+                                                                Vector3.Distance(guitarPoint.transform.position, otherObject.transform.position))) {
+                    otherObject.GetComponent<Ranged_Enemy>().takeDamage(damage);
+                }
+            }
+            //----------------------------------------------------------------------------------
+            else if (otherObject.transform.tag == "Ranged_Enemy2") {
+                if (!Physics.Raycast(guitarPoint.transform.position, -(guitarPoint.transform.position - otherObject.transform.position).normalized, out hit,
+                                                                Vector3.Distance(guitarPoint.transform.position, otherObject.transform.position))) {
+                    otherObject.GetComponent<Ranged_Enemy2>().takeDamage(damage);
+                }
+            }
+            //----------------------------------------------------------------------------------
+            else if (otherObject.transform.tag == "Chaser") {
+                if (Physics.Raycast(guitarPoint.transform.position, -(guitarPoint.transform.position - otherObject.transform.position).normalized, out hit,
+                                                                Vector3.Distance(guitarPoint.transform.position, otherObject.transform.position))) {
+                    otherObject.GetComponent<Chaser>().takeDamage(damage);
+                }
+            }
+        }
+    }
+
+    public void moveForward(float distance) {
+        transform.Translate(new Vector3(distance, 0, 0), Space.Self);
+    }
+}
